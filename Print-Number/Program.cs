fcdf011bb5980c6aa10dfa19eea048de76f6feeb
JerriemyPascual <v-jerriemypa@cloudstaff.com>
2022-09-28 15:22:16 +0800

    Add project files.

---
+++ Print-Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            i = 0;
+            j = 0;
+            for (i = 1; i <= 6; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
